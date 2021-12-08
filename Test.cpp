8af0b4e1decc5fd203b499b9cc8f57b80c730424
bradya2 <95786446+bradya2@users.noreply.github.com>
2021-12-08 15:30:36 -0600

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+  int counter;
+  
+  counter = 0;
+  
+  for (int i = 0; i < 10; i++)
+  {
+    cout << "Loop Number is " << counter;
+    counter++;
+  }
+  
+  return 0;
+}
